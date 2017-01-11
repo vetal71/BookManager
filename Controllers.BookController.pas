e2138b371cc5e36849cc3a4b05a3b69c51c2dcc3
vetal71 <vetalkolesnik@gmail.com>
2017-01-11 18:02:04 +0300

    Added some class

--- Controllers.BookController.pas
+++ Controllers.BookController.pas
@@ -2,6 +2,34 @@ unit Controllers.BookController;
 
 interface
 
+uses
+  Entities.Book;
+
+type
+  TBookController = class    
+  public
+    procedure SaveBook(Book: TBook);
+  end;
+
 implementation
+
+uses
+  Common.DBConnection, 
+  Aurelius.Engine.ObjectManager;
+
+{ TBookController }
+
+procedure TBookController.SaveBook(Book: TBook);
+var
+  Manager: TObjectManager;
+begin
+  Manager := TDBConnection.GetInstance.CreateObjectManager;
+  try
+    Manager.SaveOrUpdate(Book);
+    Manager.Flush;
+  finally
+    Manager.Free;
+  end;
+end;
 
 end.
